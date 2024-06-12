d3644f18d3a54f2e3e2a19649eb1fdb90ec16982
gayathri <gayathrithota66@gmail.com>
2024-06-12 21:26:25 +0530

    day 2 complete

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import os
 import eel
 
-from engine.features import *
-from engine.command import *
-
 def start():
     
     eel.init("www")
 
-    playAssistantSound()
 
 
     os.system('start msedge.exe --app="http://localhost:8000/index.html"')
